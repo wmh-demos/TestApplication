09785296ec94e2e751ac040252e462570a3b5f62
weimuhua <weimuhua0812@126.com>
2017-10-19 20:31:51 +0800

    修改插件APK

--- app/src/main/java/wayne/me/testapplication/MainActivity.java
+++ app/src/main/java/wayne/me/testapplication/MainActivity.java
@@ -1,13 +1,14 @@
 package wayne.me.testapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Great!I'm a apk which not installed!", Toast.LENGTH_LONG).show();
     }
 }
