c33ba234e69b8e0bc0d095bb2221bb12690e0466
weimuhua <weimuhua0812@126.com>
2017-10-19 16:02:19 +0800

    initial commit.

---
+++ app/src/main/java/wayne/me/testapplication/Main.java
@@ -0,0 +1,11 @@
+package wayne.me.testapplication;
+
+/**
+ * Created by wayne on 2017/10/19.
+ */
+
+public class Main {
+    public String getMessage() {
+        return "Hello, I'm Java code in APK file .";
+    }
+}
